6065834902e9bebf0a130b9d4a558f8e360208a7
Fabian Weber <fabian.weber@uzh.ch>
2014-11-04 08:09:29 +0100

    Commit

--- AgrarAlpha_v1/src/com/gwt/client/ImportHandler.java
+++ AgrarAlpha_v1/src/com/gwt/client/ImportHandler.java
@@ -1,7 +1,12 @@
 package com.gwt.client;
 
-public class ImportHandler {
-	
-	
-	
+import com.google.appengine.api.utils.SystemProperty;
+import com.mysql.jdbc.Driver;
+
+public class ImportHandler {}
+
+
+public static void main(String[] args) {
+	final ImportHandler hello;
+	hello.connect();
 }
