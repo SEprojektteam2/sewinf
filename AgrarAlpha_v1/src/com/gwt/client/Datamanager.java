de8063bd17b6c0f313dbb189d430004b6a2b9983
WilliamMartini <william.martini@outlook.com>
2014-11-07 11:04:29 +0100

    rename

---
+++ AgrarAlpha_v1/src/com/gwt/client/Datamanager.java
@@ -0,0 +1,40 @@
+package com.gwt.client;
+
+import java.sql.Connection;
+
+import com.google.gwt.visualization.client.DataTable;
+import com.gwt.server.MySQLConnection;
+
+public class Datamanager {
+	
+	//Create connection to mysqldatabase
+	private void connectToDatabase(){ 
+		MySQLConnection database = new MySQLConnection("173.194.253.240:3306","root","","agrar","agraralphav1:agrar");
+	    if(database.connect()){
+	    	System.out.println("<html><head></head><body>Connection Started</body></html>");
+	    }
+	    Connection conn = database.returnConnection();
+	}
+	
+	private void calculateInterpolation(){
+		//evt. Klassse die die Berechnung übernimmt
+		//Achtung Zahl sind String
+	}
+	
+	
+	public void getData(String country, String product, String type){
+		//private DataTable TableDATA;
+		//-> siehe prepareData() VisualizationManager
+		
+	}
+	
+	public void saveUser(String username, String param1, String param2){
+		
+	}
+	
+	public void getUser(String username){
+		
+	}
+	
+	
+}
