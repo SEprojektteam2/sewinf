23e67cecbe424a0c8143d2e2cbefc46dc42fdbc6
WilliamMartini <william.martini@outlook.com>
2014-11-07 10:22:35 +0100

    b

--- AgrarAlpha_v1/src/com/gwt/client/Datenmanager.java
+++ AgrarAlpha_v1/src/com/gwt/client/Datenmanager.java
@@ -1,5 +1,16 @@
 package com.gwt.client;
 
-public class Datenmanager {
+import java.sql.Connection;
 
+import com.gwt.server.MySQLConnection;
+
+public class Datenmanager {
+	private void connectToDatabase(){
+		
+		MySQLConnection database = new MySQLConnection("173.194.253.240:3306","root","","agrar","agraralphav1:agrar");
+	    if(database.connect()){
+	    	System.out.println("<html><head></head><body>Connection Started</body></html>");
+	    }
+	    Connection conn = database.returnConnection();
+	}
 }
