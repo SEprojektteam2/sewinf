b95a347344fff48700bd05ce2dcce64463360851
WilliamMartini <william.martini@outlook.com>
2014-11-07 10:56:33 +0100

    b

--- AgrarAlpha_v1/src/com/gwt/client/Datenmanager.java
+++ AgrarAlpha_v1/src/com/gwt/client/Datenmanager.java
@@ -5,12 +5,33 @@ import java.sql.Connection;
 import com.gwt.server.MySQLConnection;
 
 public class Datenmanager {
-	private void connectToDatabase(){
-		
+	
+	//Create connection to mysqldatabase
+	private void connectToDatabase(){ 
 		MySQLConnection database = new MySQLConnection("173.194.253.240:3306","root","","agrar","agraralphav1:agrar");
 	    if(database.connect()){
 	    	System.out.println("<html><head></head><body>Connection Started</body></html>");
 	    }
 	    Connection conn = database.returnConnection();
 	}
+	
+	private void calculateInterpolation(){
+		//evt. Klassse die die Berechnung übernimmt
+		//Achtung Zahl sind String
+	}
+	
+	
+	public void getDataBy(String param1, String param2){
+		
+	}
+	
+	public void saveUser(String username, String param1, String param2){
+		
+	}
+	
+	public void getUser(String username){
+		
+	}
+	
+	
 }
