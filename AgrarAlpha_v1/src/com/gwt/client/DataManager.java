1e4ddcd285cb3c109395070e8433910e5919d8a4
WilliamMartini <william.martini@outlook.com>
2014-11-09 22:13:50 +0100

    the kingdom far far away

--- AgrarAlpha_v1/src/com/gwt/client/DataManager.java
+++ AgrarAlpha_v1/src/com/gwt/client/DataManager.java
@@ -1,11 +1,16 @@
 package com.gwt.client;
 
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;/**/
 
 import com.google.gwt.visualization.client.DataTable;
-import com.gwt.server.MySQLConnection;
+import com.gwt.client.MySQLConnection;
 import com.google.appengine.api.utils.SystemProperty;
-import com.mysql.jdbc.Connection;
+//import com.google.cloud.sql.jdbc.ResultSet;
+import com.mysql.jdbc.*;
 
 import java.util.ArrayList;
 
@@ -27,8 +32,8 @@ public class DataManager {
 	}
 	
 	
-	private ArrayList<String> readDatabase(String query){
-		ArrayList<String> result = new ArrayList<String>();
+	private ArrayList<String[]> readDatabase(String query, String searchingVar, String outputVar){
+		ArrayList<String[]> result = new ArrayList<String[]>();
 		// create the java statement
 		Statement st = null;
 		try {
@@ -43,7 +48,10 @@ public class DataManager {
 				
 			while (rs.next())
 			{
-				String resultTemp = rs.getString("result");
+				String[] resultTemp = new String[3];
+				resultTemp[0] = rs.getString(searchingVar);
+				resultTemp[1] = rs.getString("Year");
+				resultTemp[2] = rs.getString(outputVar);
 				result.add(i, resultTemp);
 				i++;
 			}
@@ -60,28 +68,31 @@ public class DataManager {
 		//private DataTable TableDATA;
 		//-> siehe prepareData() VisualizationManager
 		//evt. absteigend nach jahr sortieren und über geben
+		// Achtung Import: Import Quantity gleich für Export!!!
 		
-		ArrayList<String> result = new ArrayList<String>();
+		ArrayList<String[]> result = new ArrayList<String[]>();
 		DataTable[] DATA = new DataTable[0];
 		
 		connectToDatabase();
-		// if you only need a few columns, specify them by name instead of using "*"
 		
 		String query="null";
+		String searchingVar="null";
+		String outputVar="null";
 		
-		if(country=="null"){// country=null when world is selected 
-			
-			
-			query = "SELECT "+query+" distinct AreaName FROM agrar ORDER BY YEAR ASC";
+		// country=null when world is selected and product is given and type is given => Output: Country + Year + Value
+		if(country=="null"){ 
+			query = "SELECT AreaName, Year, Value FROM agrar WHERE ElementName = '"+type+"' AND ItemName = '"+product+"' ORDER BY Year ASC";
 		}
+		//
 		if(product=="null"){
-			query = "SELECT distinct AreaName FROM agrar WHERE Domain = Annual population";
+			query = "SELECT ItemName, Year, Value FROM agrar WHERE ElementName = '"+type+"' AND AreaName = '"+country+"' ORDER BY Year ASC";
 		}
+		//
 		if(type=="null"){
-			query = "SELECT distinct AreaName FROM agrar WHERE Domain = Annual population";
+			query = "SELECT ElementName, Year, Value FROM agrar WHERE ItemName = '"+product+"' AND AreaName = '"+country+"' ORDER BY Year ASC";
 		}
 		
-		result = readDatabase(query);
+		result = readDatabase(query,searchingVar,outputVar);
 		
 		return DATA;
 		
@@ -91,13 +102,35 @@ public class DataManager {
 		//Array mit Strings als Rückgabe
 		//limit 1 bei der Abfrage (entfernt die Dupletten)
 		ArrayList<String> countries = new ArrayList<String>();
-		countries.add(0, "World");
+		//countries.add(0, "World");
 		
 		connectToDatabase();
 		// if you only need a few columns, specify them by name instead of using "*"
-		String query = "SELECT distinct AreaName FROM agrar WHERE Domain = Annual population";
+		String query = "SELECT distinct AreaName FROM agrar WHERE Domain = 'Annual population'";
 		
-		countries = readDatabase(query);
+		// create the java statement
+		Statement st = null;
+		try {
+			st = conn.createStatement();
+			
+			// execute the query, and get a java resultset
+			ResultSet rs = null;
+			rs = st.executeQuery(query);
+					
+			// iterate through the java resultset
+			int i=0;
+				
+			while (rs.next())
+			{
+				String resultTemp = rs.getString("AreaName");
+				countries.add(i, resultTemp);
+				i++;
+			}
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return (countries);
 	}
