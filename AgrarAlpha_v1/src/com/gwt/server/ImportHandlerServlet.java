26754061808e8d1ec5e262f6ef88f0a50547486e
fabianweber <fabian.weber@uzh.ch>
2014-11-17 00:27:13 +0100

    Many improvements.. :)

--- AgrarAlpha_v1/src/com/gwt/server/ImportHandlerServlet.java
+++ AgrarAlpha_v1/src/com/gwt/server/ImportHandlerServlet.java
@@ -4,144 +4,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import com.google.appengine.api.ThreadManager;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreInputStream;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
-import com.google.appengine.api.utils.SystemProperty;
+
 import com.opencsv.CSVReader;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//
-//Import external class for simple csv handling
 
-
-	@SuppressWarnings("serial")
-	public class ImportHandlerServlet extends HttpServlet {
+@SuppressWarnings("serial")
+public class ImportHandlerServlet extends HttpServlet {
+		
 		public static final Logger log = Logger.getLogger(ImportHandlerServlet.class.getName());
 		private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();  	
-		private int noOfThreads =0;
+		private int noOfThreads = 0;
 		
 		@Override
 		public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+			//setting up outputstream
 			PrintWriter out = resp.getWriter();
-			Date start = new Date();
-			
 			resp.setContentType("text/html");
 			
+			//get the file from blobstore
 			Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
 			List<BlobKey> bkList = blobs.get("importCSV");
 			BlobKey blobKey = bkList.get(0);
 			BlobstoreInputStream blobStream = new BlobstoreInputStream(blobKey);
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(blobStream, "utf-8"));
-			ArrayList<String[][]> arrays= new ArrayList<String[][]>(); 
 			CSVReader csvReader = null;
-			String url = null;
-		
-		    MySQLConnection database = new MySQLConnection("173.194.253.240:3306","root","","agrar","agraralphav1:agrar");
-		    if(database.connect())
-		    	out.println("Connection started!");
-		    Connection conn = database.returnConnection();
-			try {
-				conn = DriverManager.getConnection(url);
-			} catch (SQLException e2) {
-				// TODO Auto-generated catch block
-				out.println(e2.toString());
-			}
-		    //TEst
-			//Connection conn = database.returnConnection();
-			//Print out first line to check if BufferedReader is empty
+			//read lines from file
 			while(bufferedReader.read() != -1){
-			
-			//New CSVReader object reading from bufferedReader
-			csvReader = new CSVReader(bufferedReader);
-			
-			//Add csv-Fields to two-dimensional array 
-			List<String[]> rows = csvReader.readAll();
-			//String output[][];
-			arrays.add(rows.toArray(new String[rows.size()][]));
-			}
-			//out.println(output.length + " " + rows.size() +" AND " + output[1].length);
-			///:)
-			csvReader.close();
-			 out.println("success");
-				//out.flush();
-				//out.close();
-			 blobstoreService.delete(blobKey);
+				csvReader = new CSVReader(bufferedReader);
+				List<String[]> rows = csvReader.readAll();
+				Thread thread = ThreadManager.createBackgroundThread(new ImportHandlerThread(rows.toArray(new String[rows.size()][])));
+				
+				//Send redirect to client
+				out.println("success");
 				out.flush();
-			 //resp.sendRedirect("http://default.agraralpha.appspot.com/");
-			 
-				String statement = "INSERT INTO records (domainCode, domain, areaCode, areaName, elementCode, elementName, itemCode, itemName, year, unit, value, flag, flagD) VALUES( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? )";
-				PreparedStatement stmt = null;
-			  try {
-				  stmt = conn.prepareStatement(statement);
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-    		for(int v = 0; v < arrays.size(); v++){
-    		
-			  String output[][] = arrays.get(v);
-			
-				for(int y = 0; y<output.length-2; y++){
-
-					  try {
-						stmt.setString(1, output[y][0]);
-						stmt.setString(2,output[y][1]);
-			          stmt.setString(3,output[y][2]);
-			          stmt.setString(4,output[y][3]);
-			          stmt.setString(5,output[y][4]);
-			          stmt.setString(6,output[y][5]);
-			          stmt.setString(7,output[y][6]);
-			          stmt.setString(8,output[y][7]);
-			          stmt.setString(9,output[y][8]);
-			          stmt.setString(10,output[y][9]);
-			          stmt.setString(11,output[y][10]);
-			          stmt.setString(12,output[y][11]);
-			          stmt.setString(13,output[y][12]);
-			          stmt.addBatch();
-					  } catch (SQLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-			         
-				}
-			
-				}
-    		try {
-
-				stmt.executeBatch();
+				resp.sendRedirect("default.agraralphav1.appspot.com");
 				
-    			String query = "DELETE FROM records WHERE domainCode = 'Domain Code' AND domain = 'Domain' AND areaCode = 'AreaCode' AND areaName = 'AreaName' AND elementCode = 'ElementCode' AND elementName = 'ElementName' AND itemCode = 'ItemCode' AND itemName = 'ItemName' AND year = 'Year' AND unit = 'Unit' AND value = 'Value' AND flag = 'Flag' AND flagD = 'FlagD'";
-    		    PreparedStatement preparedStmt = conn.prepareStatement(query);
-    		    preparedStmt.execute();
-    			
+				//add new thread to insert lines into database. There are only 10 Threads allowed.
+				while(noOfThreads > 9){
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						log.warning(e.toString());
+					}
+				}
+					
+				thread.start();
+				noOfThreads++;
+					
+				try {
+					thread.join();
+				} catch (InterruptedException e) {
+					log.warning(e.toString());
+				}
 				
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				if(!thread.isAlive())
+					noOfThreads--;
 			}
-    		Date end = new Date();
-    		long timeElapsed = end.getTime()-start.getTime();
-    		log.severe("Zeit: " + Objects.toString(timeElapsed));
-		}}
-    		
-
-		
-
+			
+			blobstoreService.delete(blobKey);
+			out.flush();
+			csvReader.close();
+		}
+	}
