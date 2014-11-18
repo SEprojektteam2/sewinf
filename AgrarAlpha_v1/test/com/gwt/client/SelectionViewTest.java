612c25c5f0541297ee32ab45cb6ea089d597d6a4
rpernischova <romana.pernischova@uzh.ch>
2014-11-18 11:47:34 +0100

    some updates

--- AgrarAlpha_v1/test/com/gwt/client/SelectionViewTest.java
+++ AgrarAlpha_v1/test/com/gwt/client/SelectionViewTest.java
@@ -2,26 +2,37 @@ package com.gwt.client;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
 public class SelectionViewTest extends GWTTestCase {
 
+	SelectionView_v2 sview;
+	
+	@Before
+	public void gwtSetUp()
+	{
+		sview = new SelectionView_v2(new MainView());
+	}
+	
 	@Test
 	public void test() {
 		
-		SelectionView.productLB.setItemSelected(1, true);
-        assertEquals("true",SelectionView.productCB.getValue());
+		SelectionView_v2.productLB.setItemSelected(1, true);
+		//Problem: setting this programmaticly doesn't fire the changeEvent!!!
+		//which would be essential for this check!!
+        assertEquals("true",SelectionView_v2.productCB.getValue());
 	
-        SelectionView.productLB.setItemSelected(0, false);
-        assertEquals("false",SelectionView.productCB.getValue());
+        SelectionView_v2.productLB.setItemSelected(0, false);
+        assertEquals("false",SelectionView_v2.productCB.getValue());
 	
-        SelectionView.typeLB.setItemSelected(1, true);
-        assertEquals("true",SelectionView.typeCB.getValue());
+        SelectionView_v2.typeLB.setItemSelected(1, true);
+        assertEquals("true",SelectionView_v2.typeCB.getValue());
 	
-        SelectionView.typeLB.setItemSelected(0, false);
-        assertEquals("false",SelectionView.typeCB.getValue());
+        SelectionView_v2.typeLB.setItemSelected(0, false);
+        assertEquals("false",SelectionView_v2.typeCB.getValue());
 	}
 
 	@Override
